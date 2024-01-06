043c85c20f4230f6a6e96723e547384b47dd112c
AsmaA5678 <151561803+AsmaA5678@users.noreply.github.com>
2024-01-06 18:59:48 +0100

    Create UselessArticleException.java

---
+++ Exceptions /UselessArticleException.java
@@ -0,0 +1,11 @@
+package mainClasses;
+
+public class UselessArticleException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public UselessArticleException(String message) {
+		super(message);
+	}
+
+}
