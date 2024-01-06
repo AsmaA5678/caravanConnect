02d5a292d391c77be9a076061ac549d0afdb0179
AsmaA5678 <151561803+AsmaA5678@users.noreply.github.com>
2024-01-06 18:31:16 +0100

    Create Habitant.java

---
+++ Habitant.java
@@ -0,0 +1,65 @@
+//import java.util.ArrayList;
+import java.util.Date;
+
+//import java.util.Date;
+
+public class Habitant extends Personne {
+	
+	private int idHabitant;
+	private Famille famille;
+	private Character genre; // f ou m
+	private String profession; 
+	private String etatSanitaire; // sain ou malade
+
+	public Beneficiaire(int idHabitant, String nom, String prenom, Date dateNaissance, int numTelephone, Famille famille, Character genre, String profession, String etatSanitaire) {
+		super(nom, prenom, dateNaissance, numTelephone);
+		this.setIdHabitant(idHabitant);
+		this.famille = famille;
+		this.genre = genre;
+		this.setProfession(profession);
+		this.etatSanitaire = etatSanitaire;
+	}
+
+	public Famille getFamille() {
+		return famille;
+	}
+
+	public void setFamille(Famille famille) {
+		this.famille = famille;
+		famille.membresFamille.add(this);
+	}
+
+	public Character getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Character genre) {
+		this.genre = genre;
+	}
+
+	public String getEtatSanitaire() {
+		return etatSanitaire;
+	}
+
+	public void setEtatSanitaire(String etatSanitaire) {
+		this.etatSanitaire = etatSanitaire;
+	}
+
+	public int getIdHabitant() {
+		return idHabitant;
+	}
+
+	public void setIdHabitant(int idHabitant) {
+		this.idHabitant = idHabitant;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	
+	
+}
