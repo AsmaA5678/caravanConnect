41ad887cddaf413e8d3717dac85a68e8f2ff874f
AsmaA5678 <151561803+AsmaA5678@users.noreply.github.com>
2024-01-06 22:07:05 +0100

    Update Famille.java

--- Classes /Famille.java
+++ Classes /Famille.java
@@ -21,14 +21,35 @@ public class Famille {
 		this.idFamille = idFamille;
 	}
 
-	// fonction qui vérifie si une famille contient des enfants
-	public boolean contientEnfants() {
-		for (int i = 0; i < this.membresFamille.size(); i++) {
-			//if (this.membresFamille.get(i).getAge()<=12)
-				return true;
-		}
-		return false;
+	
+	// fonction qui vérifie si une famille ne contient pas une persone qui appartient a une categorie d'age
+	public boolean estSansMembresDansCategorie(int ageMin, int ageMax) {
+	    for (Beneficiaire membre : this.membresFamille) {
+	        if (membre.getAge() >= ageMin && membre.getAge() <= ageMax) {
+	            return false;  // Il y a au moins un membre dans la catégorie d'âge spécifiée
+	        }
+	    }
+	    return true;  // Aucun membre dans la catégorie d'âge spécifiée
+	}
+	
+	public boolean estSansEnfants() { //famille sans enfants
+	    return estSansMembresDansCategorie(0, 12);
 	}
 
+	public boolean estSansAdolescent() { //famille sans adolescent
+	    return estSansMembresDansCategorie(13, 17);
+	}
+
+	public boolean estSansJeuneAdulte() {//famille sans jeune adulte
+	    return estSansMembresDansCategorie(18, 24);
+	}
+
+	public boolean estSansAdulte() {//famille sans adulte
+	    return estSansMembresDansCategorie(25, 64);
+	}
+
+	public boolean estSansPersonneAgee() {//famille sans agée
+	    return estSansMembresDansCategorie(65, Integer.MAX_VALUE);
+	}
 	
 }
