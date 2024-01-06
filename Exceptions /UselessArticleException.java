a5f926705fb2e68e64c666fa18be6527b57101f1
AsmaA5678 <151561803+AsmaA5678@users.noreply.github.com>
2024-01-06 19:45:18 +0100

    Update UselessArticleException.java

--- Exceptions /UselessArticleException.java
+++ Exceptions /UselessArticleException.java
@@ -1,11 +1,13 @@
 package mainClasses;
-
 public class UselessArticleException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 
 	public UselessArticleException(String message) {
 		super(message);
+		System.err.println("Une exception UselessArticleException a été levée:"+ message);
 	}
 
 }
+
+
