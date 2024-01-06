74c05ecbb238ff48b0c6f6348aad897670b42320
AsmaA5678 <151561803+AsmaA5678@users.noreply.github.com>
2024-01-07 00:36:12 +0100

    Update Article.java

--- Articles /Article.java
+++ Articles /Article.java
@@ -4,6 +4,7 @@ abstract class Article {
 	
 	private int idArticle; 
 	protected Paquet paquet;
+	List<Paquet> tousLesPaquets = new ArrayList<>();
 		
 	public Article(int idArticle) {
 		this.idArticle = idArticle;
@@ -23,6 +24,11 @@ abstract class Article {
 	}
 	
 	public void setPaquet(Paquet paquet) throws UselessArticleException {
+		 for (Paquet autrePaquet : tousLesPaquets) {
+		        if (autrePaquet.listeArticles.contains(this)) {
+		            throw new UselessArticleException("L'article est déjà dans un autre paquet.");
+		        }
+		    }
 		this.paquet = paquet;
 		paquet.listeArticles.add(this);
 	}
