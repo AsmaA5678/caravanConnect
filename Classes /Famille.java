9972c6cb0b71e3abbce2516280e6e6a46a96acf3
AsmaA5678 <151561803+AsmaA5678@users.noreply.github.com>
2024-01-06 21:59:23 +0100

    Update Famille.java

--- Classes /Famille.java
+++ Classes /Famille.java
@@ -1,67 +1,4 @@
- package mainClasses;
-import java.util.Date;
-
-public class Habitant extends Personne {
-	
-	private int idHabitant;
-	private Famille famille;
-	private Character genre; // f ou m
-	private String profession; 
-	private String etatSanitaire; // sain ou malade
-
-	public Habitant(int idHabitant, String nom, String prenom, String dateNaissance, int numTelephone, Famille famille, Character genre, String profession, String etatSanitaire) {
-		super(nom, prenom, dateNaissance, numTelephone);
-		this.setIdHabitant(idHabitant);
-		this.famille = famille;
-		this.genre = genre;
-		this.setProfession(profession);
-		this.etatSanitaire = etatSanitaire;
-	}
-
-	public Famille getFamille() {
-		return famille;
-	}
-
-	public void setFamille(Famille famille) {
-		this.famille = famille;
-		famille.membresFamille.add(this);
-	}
-
-	public Character getGenre() {
-		return genre;
-	}
-
-	public void setGenre(Character genre) {
-		this.genre = genre;
-	}
-
-	public String getEtatSanitaire() {
-		return etatSanitaire;
-	}
-
-	public void setEtatSanitaire(String etatSanitaire) {
-		this.etatSanitaire = etatSanitaire;
-	}
-
-	public int getIdHabitant() {
-		return idHabitant;
-	}
-
-	public void setIdHabitant(int idHabitant) {
-		this.idHabitant = idHabitant;
-	}
-
-	public String getProfession() {
-		return profession;
-	}
-
-	public void setProfession(String profession) {
-		this.profession = profession;
-	}
-	
-	
-}
-[6:34 PM, 06/01/2024] .: package mainClasses;
+package mainClasses;
 import java.util.ArrayList;
 
 public class Famille {
@@ -84,37 +21,14 @@ public class Famille {
 		this.idFamille = idFamille;
 	}
 
-	// fonction qui vérifie si une famille ne contient pas une persone qui appartient a une categorie d'age
-	public boolean estSansMembresDansCategorie(int ageMin, int ageMax) {
-	    for (Beneficiaire membre : this.membresFamille) {
-	        if (membre.getAge() >= ageMin && membre.getAge() <= ageMax) {
-	            return false;  // Il y a au moins un membre dans la catégorie d'âge spécifiée
-	        }
-	    }
-	    return true;  // Aucun membre dans la catégorie d'âge spécifiée
+	// fonction qui vérifie si une famille contient des enfants
+	public boolean contientEnfants() {
+		for (int i = 0; i < this.membresFamille.size(); i++) {
+			//if (this.membresFamille.get(i).getAge()<=12)
+				return true;
+		}
+		return false;
 	}
-	
-	public boolean estSansEnfants() { //famille sans enfants
-	    return estSansMembresDansCategorie(0, 12);
-	}
-
-	public boolean estSansAdolescent() { //famille sans adolescent
-	    return estSansMembresDansCategorie(13, 17);
-	}
-
-	public boolean estSansJeuneAdulte() {//famille sans jeune adulte
-	    return estSansMembresDansCategorie(18, 24);
-	}
-
-	public boolean estSansAdulte() {//famille sans adulte
-	    return estSansMembresDansCategorie(25, 64);
-	}
-
-	public boolean estSansPersonneAgee() {//famille sans agée
-	    return estSansMembresDansCategorie(65, Integer.MAX_VALUE);
-	}
-
-
 
 	
 }
